35b480b4f50b158b81359a9bf8c1e8fa878ff759
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2015-12-08 11:57:37 -0700

    created autoresponse module for days of the week

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 //This is where you should incorporate your modules and execute your app
+var fs = require('fs');
+var respond = require('./autoResponse.js');
+respond.AutoResponse("Friday");
--- autoResponse.js
+++ autoResponse.js
@@ -1,1 +1,30 @@
 //Write your Constructor Function that will generate a different response for each day of the week
+module.exports = {
+  AutoResponse:function(string) {
+    switch (string) {
+      case "Monday":
+        console.log("I hate Mondays");
+        break;
+      case "Tuesday":
+        console.log("Tuesdays aren't great");
+        break;
+      case "Wednesday":
+        console.log("Wednesdays are OK");
+        break;
+      case "Thursday":
+        console.log("The weekend is almost here!");
+        break;
+      case "Friday":
+        console.log("Fridays are great!");
+        break;
+      case "Saturday":
+        console.log("Saturdays are the best!");
+        break;
+      case "Sunday":
+        console.log("Sundays are pretty nice!");
+        break;
+      default:
+        console.log("That's not a day");
+    }
+  }
+};
